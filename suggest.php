eb199bfd3b4e40762a58de66b65228aa466819df
Amer Krlicbegovic <amer.krlicbegovic@gmail.com>
2017-02-02 01:01:43 +0100

    Initial commit

---
+++ suggest.php
@@ -0,0 +1,10 @@
+<?php
+$pageTitle = "Suggest a media Item";
+$section = "suggest";
+include("inc/header.php"); ?>
+
+<div class="section page">
+    <h1>Suggest a media Item</h1>
+</div>
+
+<?php include("inc/footer.php"); ?>
