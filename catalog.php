b211654c2df0f6b446b3e964e34e6f387ee6bda1
Amer Krlicbegovic <amer.krlicbegovic@gmail.com>
2017-02-02 16:09:10 +0100

    Funkcija za random biranje

--- catalog.php
+++ catalog.php
@@ -22,12 +22,18 @@ include ("inc/header.php"); ?>
 
     <div class="section catalog page">
         <div class="wrapper">
-            <h1><?php echo $pageTitle; ?></h1>
-            <ul class="items">
-                <?php foreach($catalog as $id => $item) {
-                    echo get_item_html($id, $item);
+            <h1><?php
+                if($section != null){
+                    echo "<a href='catalog.php'>Full Catalog</a> &gt;";
                 }
-                    ?>
+                echo $pageTitle; ?></h1>
+            <ul class="items">
+                <?php
+                $categories = array_category($catalog, $section);
+                foreach ($categories as $id => $item) {
+                    echo get_item_html($id, $catalog[$item]);
+                }
+                ?>
             </ul>
         </div>
 
