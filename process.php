f8372b45966006524b6fb9eb429ec0f0a34376ed
Amer Krlicbegovic <amer.krlicbegovic@gmail.com>
2017-02-03 14:01:44 +0100

    Funkcija za random biranje

--- process.php
+++ process.php
@@ -12,15 +12,8 @@ echo "$email_body";
 echo "</pre>";
 
 
-$pageTitle = "Thank you";
-$selection = null;
+//To Do: Send email
 
-include("inc/header.php") ?>
-<div class="section page">
-    <h1>Thank you</h1>
-    <p>Thanks for the email!</p>
-</div>
-
-
-<?php include("inc/footer.php") ?>
+header("location:thanks.php");
+?>
 
